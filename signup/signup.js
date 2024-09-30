2452c182cff2ec654c74d0178e50a56bfc4b995e
Atharva Mishra <atharvamishramain@gmail.com>
2024-09-30 12:15:31 +1000

    finished signup design

---
+++ signup/signup.js
@@ -0,0 +1,22 @@
+login_div = document.getElementById("cover");
+email = document.getElementById("email");
+password = document.getElementById("password");
+remember = document.getElementById("remember");
+
+function login_show() {
+    login_div.style.display = "flex";
+    email.value = "";
+    password.value = "";
+    remember.checked = false;
+}
+
+document.getElementById("close").addEventListener("click", () => {
+    login_div.style.display = "none";
+    email.value = "";
+    password.value = "";
+    remember.checked = false;
+});
+
+window.onload = function() {
+    document.body.style.visibility = 'visible';
+};

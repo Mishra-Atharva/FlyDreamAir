ff83d40502cabd771b4fcc301f059e882b18f072
Atharva Mishra <atharvamishramain@gmail.com>
2024-10-02 12:56:32 +1000

    added json file

--- script.js
+++ script.js
@@ -20,3 +20,47 @@ document.getElementById("close").addEventListener("click", () => {
 window.onload = function() {
     document.body.style.visibility = 'visible';
 };
+
+
+function makeAJAXQuery() {
+
+    // creates an XMLHttpRequest
+    var xhttp = new XMLHttpRequest();
+
+    // creates an handler for the readyState change
+    xhttp.onreadystatechange = function () {
+
+        // Calling readSateChangeHandler function
+        readyStateChangeHandler(xhttp);
+    };
+
+    // Gets JSON file
+    xhttp.open("GET", "user.json", true);
+    xhttp.send();
+}
+
+function readyStateChangeHandler(xhttp) {
+    // Checks if the status is success or failure
+    if (xhttp.readyState == 4) {
+        if (xhttp.status == 200) {
+            handleStatusSuccess(xhttp);
+        } else {
+            handleStatusFailure(xhttp);
+        }
+    }
+}
+
+// If failure display error
+function handleStatusFailure(xhttp) {
+    var display = document.getElementById("display");
+    display.innerHTML = "XMLHttpRequest failed: status " + xhttp.status;
+}
+
+// If success
+function handleStatusSuccess(xhttp) {
+    var jsonText = xhttp.responseText;
+
+    var details = JSON.parse(jsonText);
+
+    displayDetails(details);
+}
